bd2dcf005d24122405f52773b5fcea7e85600bf3
AlexJiang <yanhengjiang123@gmail.com>
2025-02-06 11:56:48 -0800

    Adding New Elements

--- week5/js/main.js
+++ week5/js/main.js
@@ -1,21 +1,33 @@
-const radiusP = document.querySelector('#radius');
-const resultP = document.querySelector('#result');
+// const radiusP = document.querySelector('#radius');
+// const resultP = document.querySelector('#result');
 
-function getNumber() {
-    let num = prompt("Enter the radius of the circle: ")
-    radiusP.textContent += num;
-    return num;
+// function getNumber() {
+//     let num = prompt("Enter the radius of the circle: ")
+//     radiusP.textContent += num;
+//     return num;
+// }
+
+// function calculateArea() {
+//     const input = getNumber();
+//     if (isNaN(input)) {
+//         alert("Please enter a number.");
+//         return;
+//     }
+//     return Math.PI * Math.pow(input, 2).toFixed(2);
+// }
+
+// const result = calculateArea();
+// resultP.textContent += result;
+
+
+let shoppingItems = ["bread", "cheese", "green pepper"];
+const shoppingList = document.querySelector(".shopping");
+
+function populateShoppingList(items, element) {
+    items.forEach(item => {
+        console.log(item);
+        let li = document.createElement("li");
+        li.textContent = item;
+        element.appendChild(li);
+    });
 }
-
-function calculateArea() {
-    const input = getNumber();
-    if (isNaN(input)) {
-        alert("Please enter a number.");
-        return;
-    }
-    return Math.PI * Math.pow(input, 2).toFixed(2);
-}
-
-const result = calculateArea();
-resultP.textContent += result;
-
